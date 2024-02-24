ff7b478a5ceeab37d49296c34dcf24144c7bd767
Darren03625 <147948500+Darren03625@users.noreply.github.com>
2024-02-24 05:43:59 +0000

    test 1

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -10,8 +10,59 @@
 #include <unistd.h> 
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+    extern char *optarg;
+    extern int optind;
+    int opt;
+    // char* iname;
+    // char* oname; 
+    // char* cname;
+    // char* pname; 
+    // char* rname;
+    int iflag = 0, oflag = 0;
+    // int cflag = 0, pflag = 0, rflag = 0;
 
-    return 1000;
+    while ((opt = getopt(argc, argv, "i:o:c:p:r:")) != -1){
+        switch (opt){
+            case 'i':
+                iflag = 1;
+                if (optarg == NULL){
+                    return MISSING_ARGUMENT;
+                }
+                // iname = optarg;
+                break;
+            case 'o':
+                oflag = 1;
+                if (optarg == NULL){
+                    return MISSING_ARGUMENT;
+                }
+                // oname = optarg;
+                break;
+            case 'c':
+                // cflag = 1;
+                if (optarg == NULL){
+                    return MISSING_ARGUMENT;
+                }
+                // cname = optarg;
+                break;
+            case 'p':
+                // pflag = 1;
+                if (optarg == NULL){
+                    return MISSING_ARGUMENT;
+                // }
+                // pname = optarg;
+                break;
+            case 'r':
+                // rflag = 1;
+                if (optarg == NULL){
+                    return MISSING_ARGUMENT;
+                }
+                break;
+                // rname = optarg;
+        }
+        if (iflag == 0 || oflag == 0){
+            return MISSING_ARGUMENT;
+        }
+    }
+    return 0;
+}
 }
